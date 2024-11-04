83fad14b9200964a1b286bc6b7d5f389e11e7dad
jackelynicole <toraljackie1638@gmail.com>
2024-11-04 17:26:16 -0500

    javascript refresher, heating/cooling, and longest name labs submission

---
+++ heatingCooling/script.js
@@ -0,0 +1,11 @@
+const actualTemp = 20;
+const desiredTemp = 23;
+
+ if actualTemp > desiredTemp {
+    console.log("Run A/C");
+ } else if (actualTemp < desiredTemp) {
+    console.log("Run heat");
+ } else {
+    console.log("Standby");
+ }
+
---
+++ javaScriptRefresher/script.js
@@ -0,0 +1,25 @@
+const firstName = "jackelyn";
+const lastName = "toral";
+const fullName = firstName + " " + lastName; 
+
+console.log(fullName);
+
+let population = 633218;
+
+console.log(population);
+
+population = population * 3; 
+
+console.log(population);
+
+const javaScriptIsCool = true;
+
+const colors = ["red", "orange", "yellow", "green","blue", "purple"];
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 4; i++) {
+    console.log(i);
+}
