88d9a5a576bbcc9e6bb0cee9c83082e22814854e
jackelynicole <toraljackie1638@gmail.com>
2024-11-10 15:54:57 -0500

    shape function lab submission

---
+++ shapeFunction/script.js
@@ -0,0 +1,11 @@
+const getAreaOfCircle = (radius) => Math.PI * radius * radius;
+console.log("Area of Circle:", getAreaOfCircle(2));   
+
+const getCircumferenceOfCircle = (radius) => 2 * Math.PI * radius;
+console.log("Circumference of Circle:", getCircumferenceOfCircle(3)); 
+
+const getAreaOfSquare = (side) => side * side;
+console.log("Area of Square:", getAreaOfSquare(2));   
+
+const getAreaOfTriangle = (base, height) => 0.5 * base * height;
+console.log("Area of Triangle:", getAreaOfTriangle(2, 4));    
